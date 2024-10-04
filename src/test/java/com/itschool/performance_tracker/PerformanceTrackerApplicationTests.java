5325c7ea7ede66240982a69a2f1d920a9f154fd4
Antal Csaba <antcsab@gmail.com>
2024-10-04 16:14:03 +0300

    First push performance tracker

---
+++ src/test/java/com/itschool/performance_tracker/PerformanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itschool.performance_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
